3ef170a7af6d3341d55b391bed826a45008594eb
nandhitha-s <nandhithasakthiveltssn@gmail.com>
2024-06-03 15:36:58 +0530

    .

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { FoodsComponent } from './components/foods/foods.component';
+export const routes: Routes = [
+    {
+        path:'**',
+        component: FoodsComponent
+    },
+    {
+        path: 'foods',
+        component: FoodsComponent
+    }
+];
--- frontend/src/app/components/foods/foods.component.spec.ts
+++ frontend/src/app/components/foods/foods.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LoginComponent } from './login.component';
+import { FoodsComponent } from './foods.component';
 
-describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+describe('FoodsComponent', () => {
+  let component: FoodsComponent;
+  let fixture: ComponentFixture<FoodsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
+      imports: [FoodsComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(LoginComponent);
+    fixture = TestBed.createComponent(FoodsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
