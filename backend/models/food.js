77a50ec6906a1836a1656464afeecd7856e47f33
nandhitha-s <nandhithasakthiveltssn@gmail.com>
2024-06-10 16:05:09 +0530

    .

--- backend/models/food.js
+++ backend/models/food.js
@@ -1,6 +1,9 @@
-var mysql = require('mysql');
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+const port = 3000;
 
-var con = mysql.createConnection({
+const con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "Aswanth2005$",
@@ -9,8 +12,16 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
   if (err) throw err;
-  con.query("SELECT * FROM posts.food", function (err, result, fields) {
+  console.log("Connected to the database!");
+});
+
+app.get('/api/posts', (req, res) => {
+  con.query("SELECT * FROM food", function (err, result, fields) {
     if (err) throw err;
-    console.log(result);
+    res.json(result);
   });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
