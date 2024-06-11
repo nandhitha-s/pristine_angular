3f6aa67f8629af2cf8ed097e41639a05c2bb0bbc
nandhitha-s <nandhithasakthiveltssn@gmail.com>
2024-06-11 17:54:07 +0530

    .

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,16 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DataDisplayComponent } from './components/data-display/data-display.component';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { HomeComponent } from './components/home/home.component';
 import { PostComponent } from './components/post/post.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-<<<<<<< HEAD
 import { NgFor } from '@angular/common';
-=======
 import { SignupComponent } from './components/signup/signup.component';
->>>>>>> f8caa308780e02dbf969a0efcffd8a2d92f1d476
 //import { HttpClient, HttpClientModule } from '@angular/common/http';
 // import { HttpModule } from '@angular/http';
 //import { HttpClientModule, HttpClient } from '@angular/common/http';
@@ -29,11 +26,9 @@ import { SignupComponent } from './components/signup/signup.component';
     HomeComponent,
     PostComponent,
     HttpClientModule,
-<<<<<<< HEAD
     NgFor,
-=======
+    NgIf,
     SignupComponent
->>>>>>> f8caa308780e02dbf969a0efcffd8a2d92f1d476
   ],
   providers: [HttpClient],
   templateUrl: './app.component.html',
