7d7a587b045f0d920afb22731206f495f48e470d
nandhitha-s <nandhithasakthiveltssn@gmail.com>
2024-06-12 14:28:31 +0530

    .

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,9 +4,10 @@ import { HomeComponent } from './components/home/home.component';
 import { PostsComponent } from './components/posts/posts.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { PostComponent } from './components/post/post.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: PostComponent },
   { path: 'posts', component: PostsComponent },
   { path: 'login', component: LoginComponent },
   { path:'signup', component: SignupComponent },
