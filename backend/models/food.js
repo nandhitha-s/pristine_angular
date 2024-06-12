87540491f6ca882e78ac7fac914b2a14a6cff327
Mithun@2303 <mithunkarthick1610@gmail.com>
2024-06-12 16:09:18 +0530

    minor fixes

--- backend/models/food.js
+++ backend/models/food.js
@@ -2,18 +2,20 @@
 
 const express = require('express');
 const router = express.Router();
-const db = require('../util/database');
+const query = require('../util/database');
 
-router.get('/api/food', (req, res, next) => {
-  const query = 'SELECT * FROM food';
-  db.query(query, (error, results) => {
-    if (error) {
-      console.error(error);
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
-    console.log(results);
-    res.json(results);
-  });
+router.get('/api/food', async(req, res, next) => {
+  const string = 'SELECT * FROM food';
+  const result = await query(string);
+  res.json(result);
+  // query(query, (error, results) => {
+  //   if (error) {
+  //     console.error(error);
+  //     return res.status(500).json({ error: 'Internal Server Error' });
+  //   }
+  //   console.log(results);
+  //   res.json(results);
+  // });
 });
 
 module.exports = router;
--- backend/util/database.js
+++ backend/util/database.js
@@ -1,9 +1,23 @@
-const mysql = require("mysql2");
+
+const mysql = require('mysql2/promise');
 const config = require("../config/config.json");
-const pool = mysql.createPool({
-  host: config.host,
-  user: config.user,
-  database: config.database,
-  password: config.password,
-});
-module.exports = pool;
+
+async function query(sql, params) {
+  try {
+    const connection = await mysql.createConnection({
+      host: config.host,
+      user: config.user,
+      database: config.database,
+      password: config.password,
+    });
+    const [results,] = await connection.execute(sql, params);
+    console.log(results);
+    return results;
+  } catch (error) {
+    
+  }
+}
+
+module.exports = {
+  query
+}
